8a0d1ce5a7f586a3b1c1b18c1b93f37ecefbf699
sey2 <54762273+sey2@users.noreply.github.com>
2021-05-21 18:23:29 +0900

    Create SelectionSort.c

---
+++ Sort/SelectionSort.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 10
+#define SWAP(x,y,t) ((t) = (x), (x) = (y), (y) = (t))
+
+int list[MAX_SIZE];
+int n;
+
+void selection_sort(int list[], int n) {
+	int i, j, least, temp;
+
+	/// O(n^)
+	for (i = 0; i < n - 1; i++) {
+		least = i;
+
+		for (j = i + 1; j < n; j++)
+			if (list[j] < list[least]) least = j;
+		if(i != least)
+			SWAP(list[i], list[least], temp);
+
+	}
+}
+
+int main(void) {
+	int i;
+	n = MAX_SIZE;
+	srand(time(NULL));
+
+	for (i = 0; i < n; i++)
+		list[i] = rand() % 100;
+
+	selection_sort(list, n);
+
+	for (i = 0; i < n; i++)
+		printf("%d ", list[i]);
+
+	printf("\n");
+	return 0;
+}
