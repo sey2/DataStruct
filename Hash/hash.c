4dbaca2d60c9b289b8133783df6b02250432709e
sey2 <54762273+sey2@users.noreply.github.com>
2021-06-11 00:54:16 +0900

    Update hash.c

--- Hash/hash.c
+++ Hash/hash.c
@@ -1,97 +1,71 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
-#define KEY_SIZE 10
-#define TABLE_SIZE 13
+#define KEY_SIZE 11
+#define TABLE_SIZE 11
 
+typedef int element;
 
-typedef struct {
-	char key[KEY_SIZE];
-}element;
+element e[TABLE_SIZE];
 
-element hash_table[TABLE_SIZE];
 
 void init_table(element ht[]) {
 	int i;
+
 	for (i = 0; i < TABLE_SIZE; i++)
-		ht[i].key[0] = NULL;
-	
+		ht[i] = 0;
 }
 
-int transform(char* key) {
-	int number = 0;
-
-	while (*key)
-		number = 31 * number + *key++;
-
-	return number;
-	
+int hash_function(int key) {
+	return key % TABLE_SIZE;
 }
 
-int hash_function(char* key) {
-	return transform(key) % TABLE_SIZE;
-}
-
-void hash_lp_add(element item, element ht[]) {
+void hash_lp_add(int item, element ht[]) {
 	int i, hash_value;
-	hash_value = i = hash_function;
+	hash_value = i = hash_function(item);
 
-	while (!(strlen(ht[i].key)==0) {
-		if (!(strcmp(item.key, ht[i].key)) {
-			fprintf(stderr, "탐색키가 중복되었습니다.\n");
-			exit(1);
+	while (ht[i] != 0) {
+		if (item == ht[i]) {
+			fprintf(stderr, "탐색키가 중복 되었습니다.\n"); return;
 		}
 		i = (i + 1) % TABLE_SIZE;
 		if (i == hash_value) {
-			fprintf(stderr, "테이블이 가득 찼습니다.\n");
-			exit(1);
+			fprintf(stderr, "테이블이 가득 찼습니다.\n"); return;
 		}
 	}
 	ht[i] = item;
 }
 
-void hash_lp_search(element item, element ht[]) {
+void hash_lp_search(int item, element ht[]) {
 	int i, hash_value;
-	hash_value = i = hash_function(item.key);
-	while (!(strlen(ht[i].key)==0)) {
-		if ((!strcmp(item, ht[i]))) {
-			fprintf(stderr, "탐색 %s 위치 = %d\n", item.key, i);
-			return;
+	hash_value = i = hash_function(item);
+	while (ht[i] != NULL) {
+		if (item == ht[i]) {
+			fprintf(stderr, "찾은 값:%d 위치 = %d\n", item, i); return;
 		}
 		i = (i + 1) % TABLE_SIZE;
 		if (i == hash_value) {
 			fprintf(stderr, "찾는 값이 테이블에 없음\n");
-			return;
 		}
 	}
+
 	fprintf(stderr, "찾는 값이 테이블에 없음\n");
 }
 
 void hash_lp_print(element ht[]) {
 	int i;
-	printf("\n======================================\n");
-	for (int i = 0; i < TABLE_SIZE; i++)
-		printf("[%d] %s\n", i, ht[i].key);
-	printf("========================================\n\n");
+	printf("\n=============================================\n");
+	for (i = 0; i < TABLE_SIZE; i++)
+		printf("인덱스: [%d] -> %d\n", i, ht[i]);
+	printf("=============================================\n");
+	
 }
 
 int main(void) {
-	char* s[7] = { "do","for","if","case","else","return","function" };
-	element e;
-
-	for (int i = 0; i < 7; i++) {
-		strcpy(e.key, s[i]);
-		hash_lp_add(e, hash_table);
-		hash_lp_print(hash_table);
+	int a[KEY_SIZE] = { 12, 44,13,88,23,94,11,39,20,16,5 };
+	for (int i = 0; i < 11; i++) {
+		hash_lp_add(a[i], e);
+		hash_lp_print(e);
 	}
-
-	for (int i = 0; i < 7; i++) {
-		strcpy(e.key, s[i]);
-		hash_lp_search(e, hash_table);
-	}
-
-	return 0;
 }
