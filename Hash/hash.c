4603f3e19f49f4ce191165d39ae152ba8bc6f85b
sey2 <54762273+sey2@users.noreply.github.com>
2021-06-11 01:18:24 +0900

    Update hash.c

--- Hash/hash.c
+++ Hash/hash.c
@@ -7,7 +7,7 @@
 
 typedef int element;
 
-element e[TABLE_SIZE];
+element hash[TABLE_SIZE];
 
 
 void init_table(element ht[]) {
@@ -40,7 +40,7 @@ void hash_lp_add(int item, element ht[]) {
 void hash_lp_search(int item, element ht[]) {
 	int i, hash_value;
 	hash_value = i = hash_function(item);
-	while (ht[i] != NULL) {
+	while (ht[i] != 0) {
 		if (item == ht[i]) {
 			fprintf(stderr, "찾은 값:%d 위치 = %d\n", item, i); return;
 		}
@@ -62,10 +62,24 @@ void hash_lp_print(element ht[]) {
 	
 }
 
+void hash_qp_add(int item, element ht[]) {
+	int i, hash_value, inc = 0;
+	i = hash_value = hash_function(item);
+	while (ht[i] != 0) {
+		if (item == ht[i]) {
+			fprintf(stderr, "탐색키가 중복 되었습니다."); return;
+		}
+		i = (hash_value + inc * inc) % TABLE_SIZE;
+		inc = inc + 1;
+	}
+	ht[i] = item;
+	
+}
+
 int main(void) {
 	int a[KEY_SIZE] = { 12, 44,13,88,23,94,11,39,20,16,5 };
 	for (int i = 0; i < 11; i++) {
-		hash_lp_add(a[i], e);
-		hash_lp_print(e);
+		hash_qp_add(a[i], hash);
+		hash_lp_print(hash);
 	}
 }
